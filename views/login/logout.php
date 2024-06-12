08262c38eed8829dbc2f930eacb9ff05c7e228a3
Alexander Pérez <alexander.k.perez96@gmail.com>
2024-06-12 16:42:20 +0200

    12/006 4:42 pm

--- views/login/logout.php
+++ views/login/logout.php
@@ -5,20 +5,25 @@ session_start();
 // Eliminar todas las variables de sesión
 $_SESSION = array();
 
-// Si se desea destruir la sesión completamente, se debe borrar también la cookie de sesión.
+// Si se desea destruir la sesión completamente, se debe borrar también la cookie de sesión
 // Nota: Esto destruirá la sesión y no la información de la sesión.
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
     );
 }
 
 // Destruir la sesión
 session_destroy();
 
-// Redirigir al usuario a la página de iniciof
+// Redirigir al usuario a la página de inicio
 header('Location: /miskyyurax/index.php');
 exit();
 ?>
